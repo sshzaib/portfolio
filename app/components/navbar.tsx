dbeedaab1cb4a41b24ca1151160b5b72e977e04e
sshzaib <shahzaib01.h@gmail.com>
2024-11-15 10:16:11 +0500

    project init

---
+++ app/components/navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  const pages = ["home", "projects", ]
+    return (
+    <div className="flex justify-center">
+      <div className=" w-1/">
+        <div>{`>_Shahzaib Haider`} </div>
+      </div>
+    </div>
+  )
+  }
